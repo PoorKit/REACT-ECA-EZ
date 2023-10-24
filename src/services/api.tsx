84189a643b34cf2d0faa766f92aa2f936a80dc83
PoorKit <zekiriel2000@gmail.com>
2023-10-24 22:13:05 +0800

    backend integration

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { User } from '../interfaces/UserInterface';
 
 const apiUrl = 'https://fakestoreapi.com';
 
@@ -22,3 +23,25 @@ export const login = async (userData: LoginRequest): Promise<LoginResponse> => {
     throw error;
   }
 };
+
+export const findMe = async (enteredUsername: string): Promise<User | null> => {
+  try {
+    const response = await axios.get(`${apiUrl}/users`);
+    const users: User[] = response.data;
+    
+    // Find the user with the entered username
+    const user = users.find((user) => user.username === enteredUsername);
+
+    if (user) {
+      return user;
+    } else {
+      return null; // User not found
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+// export const fetchProducts = async (): Promise<Product[]> => {
+  
+// }
